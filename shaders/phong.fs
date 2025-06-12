15a192dd52cfbb6d702b0fc97d7efbdf2f511770
unknown <matnun95@gmail.com>
2025-06-11 23:14:45 -0300

    Ajuste no fragment shader

--- shaders/phong.fs
+++ shaders/phong.fs
@@ -1,34 +1,47 @@
-#version 330 core
+#version 430
 
-in vec3 fragPos;
-in vec3 normal;
+in vec3 finalColor;
 in vec2 texCoord;
+in vec3 scaledNormal;
+in vec3 fragPos;
 
-uniform sampler2D diffuseTexture;
-uniform vec3 lightPos;
-uniform vec3 lightColor;
+//Propriedades da superficie
+uniform vec3 ka, ks;
+uniform float kd, q;
+
+//Propriedades da fonte de luz
+uniform vec3 lightPos, lightColor;
+
+//Propriedades da câmera
 uniform vec3 cameraPos;
 
-uniform vec3 Ka;
-uniform vec3 Ks;
-uniform float q;
-
-out vec4 FragColor;
+out vec4 color;
+//Buffer da textura
+uniform sampler2D texBuffer;
 
 void main()
 {
-    vec3 ambient = Ka * texture(diffuseTexture, texCoord).rgb;
 
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * texture(diffuseTexture, texCoord).rgb;
+    //Coeficiente luz ambiente
+    vec3 ambient = ka * lightColor;
 
-    vec3 viewDir = normalize(cameraPos - fragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), q);
-    vec3 specular = spec * Ks * lightColor;
+    //Coeficiente reflexão difusa
+    vec3 diffuse;
+    vec3 N = normalize(scaledNormal);
+    vec3 L = normalize(lightPos - fragPos);
+    float diff = max(dot(N,L),0.0);
+    diffuse = kd * diff * lightColor;
 
-    vec3 finalColor = ambient + diffuse + specular;
-    FragColor = vec4(finalColor, 1.0);
+    //Coeficiente reflexão especular
+    vec3 specular;
+    vec3 R = normalize(reflect(-L,N));
+    vec3 V = normalize(cameraPos - fragPos);
+    float spec = max(dot(R,V),0.0);
+    spec = pow(spec,q);
+    specular = ks * spec * lightColor;
+
+    vec4 texColor = texture(texBuffer,texCoord);
+    vec3 result = (ambient + diffuse) * vec3(texColor) + specular;
+
+    color = vec4(result,1.0);
 }
